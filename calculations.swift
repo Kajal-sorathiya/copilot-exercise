6742a571146e4e7c0a2e7b8869b8b55f70fcf78b
Kajal Sorathiya <65402420+Kajal-sorathiya@users.noreply.github.com>
2025-01-23 05:33:37 +0000

    Copilot first commit 🥳

---
+++ calculations.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Calculations {
+    private func calculateAdditionFor(_ number1: Int, with number2: Int) {
+        let result = number1 + number2
+        print("The result of \(number1) + \(number2) is \(result)")
+    }
+
+    private func calculateSubtractionFor(_ number1: Int, with number2: Int) {
+        let result = number1 - number2
+        print("The result of \(number1) - \(number2) is \(result)")
+    }
+}
