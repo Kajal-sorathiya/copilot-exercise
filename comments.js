fefa31803a2b018340354b0c092738a8de311a5c
Kajal Sorathiya <65402420+Kajal-sorathiya@users.noreply.github.com>
2025-01-23 06:04:58 +0000

    Adding my third commit 🥳

---
+++ comments.js
@@ -0,0 +1,11 @@
+
+// Create we server
+function start() {
+var http = require('http'); 
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Node.js</h1>');
+    res.write('<p>Learning Node.js</p>');
+    res.end();
+});
+}
